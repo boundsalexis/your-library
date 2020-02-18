e6e7b82a248ec5c108fa16308d915559e2d98c63
kqarlos <kqarlos@hotmail.com>
2020-02-18 14:40:59 -0800

    Models and controllers

--- models/index.js
+++ models/index.js
@@ -1,16 +1,18 @@
-//dependencies //straight from docs
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 //creates schema
 const bookSchema = new Schema({
-  title:  String, // String is shorthand for {type: String}
+  title:  String, 
   author: String,
   picture:   String,
   description: String,
   link: String
 });
+
 //models schema
 const Book = mongoose.model('Book', bookSchema);
+
 //exports model
 module.exports = Book;
 
