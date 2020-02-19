3385613cee47aacc92c772476bfcfe79e25197ab
kqarlos <kqarlos@hotmail.com>
2020-02-19 14:49:00 -0800

    Global State

--- client/src/components/BookSearch.js
+++ client/src/components/BookSearch.js
@@ -27,10 +27,8 @@ function BookSearch() {
                 type: SET_SEARCH_RESULTS,
                 searchResults: books
             });
-            console.log("SAVING RESULTS");
             console.log(books);
         });
-      
     }
     return (
 
@@ -46,22 +44,6 @@ function BookSearch() {
 
     );
 
-
-    render() {
-        return (
-            <form className="container" onSubmit={this.onSubmitQuery}>
-                <div className="form-group">
-                    <h2>
-                        <label htmlFor="query">Book Search</label>
-                    </h2>
-                    <h5>
-                        <label htmlFor="query">Book</label>
-                    </h5>
-                    <input type="text" className="form-control" id="query" placeholder="Enter Book" onChange={this.onChangeQuery} value={this.state.query}/>
-                </div>
-            </form>
-        );
-    }
 }
 
 export default BookSearch;
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -19,66 +19,6 @@ const reducer = (state, action) => {
                 ...state,
                 saved: action.savedResults,
             };
-        // case UPDATE_ITEMS:
-        //     return {
-        //         ...state,
-        //         searchItems: [...action.searchItems],
-        //     };
-        // case UPDATE_CATEGORIES:
-        //     return {
-        //         ...state,
-        //         categories: [...action.categories],
-        //     };
-        // case REMOVE_CART_ITEM:
-        //     var count = state.cartCount;
-        //     delete count[action.name];
-        //     return {
-        //         ...state,
-        //         cart: state.cart.filter((item) => {
-        //             return item.name !== action.name;
-        //         }),
-        //         cartCount: count
-        //     };
-        // case ADD_CART_ITEM:
-        //     var count = state.cartCount;
-        //     if (count[action.item.name]) {
-        //         count[action.item.name]++;
-        //         return {
-        //             ...state,
-        //             cartCount: count
-        //         };
-        //     } else {
-        //         count[action.item.name] = 1;
-        //         return {
-        //             ...state,
-        //             cart: [action.item, ...state.cart],
-        //             cartCount: count
-        //         };
-        //     }
-
-        // case CLEAR_CART:
-        //     return {
-        //         ...state,
-        //         cart: [],
-        //         cartCount: {}
-        //     };
-        // case SET_CURRENT_ITEM:
-        //     return {
-        //         ...state,
-        //         currentItem: action.item,
-        //     };
-        // case LOGIN:
-        //     return {
-        //         ...state,
-        //         loggedin: true,
-        //         user: action._id
-        //     };
-        // case LOGOUT:
-        //     return {
-        //         ...state,
-        //         loggedin: false,
-        //         user: ""
-        //     };
         default:
             return state;
     }
